20ecbae0007537a4f52be3481a78adc4adb11096
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-02-18 21:21:45 +0530

    epamtask4 about HouseConstructionCost

---
+++ src/NewClass.java
@@ -0,0 +1,14 @@
+package assignment4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class NewClass 
+	public void test() 
+        {
+		HouseConstructionCost test=new HouseConstructionCost();
+		double output=test.cost("high","No",5.0);
+		assertEquals(9000.0,output,0);
+	}
+
+}
